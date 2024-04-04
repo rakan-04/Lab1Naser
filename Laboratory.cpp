9f93c273c1aa01102e3489aa07eb0e08f21b8a85
RAKAN NASER <rakan-04@github.com>
2024-04-04 15:23:32 -0400

    second commit

---
+++ Laboratory.cpp
@@ -0,0 +1,24 @@
+/*
+ * Program:  First Laboratory
+ * Author:   Rakan Naser
+ * Date:     1/25/2024
+ *
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void) {
+	float width;
+	float length;
+	float area;
+	length = 4.5;
+	width = 3.35;
+	area = length * width;
+	cout << "Rectangle Area = " << area << "\n";
+
+	cout<< "Who's on First? \n";
+	return 0;
+}
